4ad0db7345de7494e6f86eb169020576dc33d663
Jeremy FELIX <jeremy.felix971@gmail.com>
2018-10-15 23:00:50 -0400

    Modification majeure sur la bdd

--- project/Honey/models.py
+++ project/Honey/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class User(models.Model):
+    login = models.CharField(max_length=32)
+    mdp   = models.CharField(max_length=32)
+    ip    = models.CharField(max_length=100)
+    useragent = models.CharField(max_length=100)
+    date  = models.DateTimeField(default = timezone.now)
+
+    class Meta:
+        verbose_name = "Login de connexion"
+        ordering = ['login']
+
+    def __str__(self):
+        return self.login
+
+
+
+class Contact(models.Model):
+    mail = models.CharField(max_length=100)
+    objet   = models.CharField(max_length=100)
+    contenu = models.TextField(null=True)
+    ip    = models.CharField(max_length=100)
+    useragent = models.CharField(max_length=100)
+    date  = models.DateTimeField(default = timezone.now)
+
+    class Meta:
+        verbose_name = "Envoie Mail"
+        ordering = ['ip']
+
+    def __str__(self):
+        return self.ip

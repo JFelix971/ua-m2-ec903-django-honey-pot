0bd4e6c7f8d505c35b1343654af285b17653fe10
Jeremy FELIX <jeremy.felix971@gmail.com>
2018-10-17 22:21:59 -0400

    Ajout du GA

--- project/project/settings.py
+++ project/project/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'Honey.context_processors.ga_tracking_id',
+                'Honey.context_processors.use_ga',
             ],
         },
     },
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GA_TRACKING_ID = 'UA-127727531-1'
+USE_GA = os.environ.get('DJANGO_USE_GA', False)
+USE_GA = {'True': True, 'False': False}.get(USE_GA, False)
